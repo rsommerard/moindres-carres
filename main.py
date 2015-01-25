6e683ed46bdeaee6066d0e4aaa8345f6e5a1ca55
Romain SOMMERARD <rsommerard@gmail.com>
2015-01-25 23:43:08 +0100

    Replace vars by functions.

--- main.py
+++ main.py
@@ -13,45 +13,40 @@ def load_data():
 
 def print_results():
   print '-' * 80
+  print 'N = {0}'.format(N)
+  print '-' * 80
   print 'x = {0}'.format(x)
   print '-' * 80
   print 'y = {0}'.format(y)
   print '-' * 80
-  print 'theta = {0}'.format(theta)
+  print 'theta = {0}'.format(theta())
   print '-' * 80
-  print 'f_theta = {0}'.format(f_theta)
+  print 'f_theta = {0}'.format(f_theta())
   print '-' * 80
-  print 'j_theta = {0}'.format(j_theta)
+  print 'j_theta = {0}'.format(j_theta())
   print '-' * 80
 
-def calcul_theta():
-  global theta
-  theta = numpy.dot(numpy.linalg.inv(numpy.dot(x, x.T)), numpy.dot(x, y))
+def theta():
+  return numpy.dot(numpy.linalg.inv(numpy.dot(x, x.T)), numpy.dot(x, y))
 
-def calcul_f_theta():
-  global f_theta
-  f_theta = numpy.dot(theta.T, x)
+def f_theta():
+  return numpy.dot(theta().T, x)
 
-def calcul_j_theta():
-  global j_theta
-  tmp = (y - numpy.dot(x.T, theta))
-  j_theta = ((1.0/N) * numpy.dot(tmp.T, tmp))
+def j_theta():
+  tmp = (y - numpy.dot(x.T, theta()))
+  return ((1.0/N) * numpy.dot(tmp.T, tmp))
 
 def print_graphs():
   pyplot.plot(t, p, '.')
-  pyplot.plot(t, f_theta)
+  pyplot.plot(t, f_theta())
   pyplot.ylabel('position (m)')
   pyplot.xlabel('temps (s)')
   pyplot.show()
 
 def main():
   load_data()
-  calcul_theta()
-  calcul_f_theta()
-  calcul_j_theta()
   print_results()
   print_graphs()
 
 if __name__ == '__main__':
   main()
-
